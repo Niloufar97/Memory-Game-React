6a9bce7b1948c73d79244dca5c0dfe830ddb78d8
Niloufar <nilooooofar97@gmail.com>
2023-12-04 20:09:15 +0100

    .

---
+++ memoryGame/src/components/cards/cardsInfo.jsx
@@ -0,0 +1,26 @@
+export const cardsInfo = [
+    {
+        id: 1,
+        name: "bear"
+    },
+    {
+        id: 2,
+        name: "crab"
+    },
+    {
+        id: 3,
+        name: "dinosaur"
+    },
+    {
+        id: 4,
+        name: "elk"
+    },
+    {
+        id: 5,
+        name: "fox"
+    },
+    {
+        id: 6,
+        name: "heldgehog"
+    },
+]
