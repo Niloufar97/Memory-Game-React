2bb9f544955d05e589cc1c99644d1d9d5b213bec
Niloufar <nilooooofar97@gmail.com>
2023-12-04 22:26:50 +0100

    flipping cards and style cards and animation

--- memoryGame/src/components/cards/Card.jsx
+++ memoryGame/src/components/cards/Card.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 import { CardsInfo } from "./cardsInfo"
 function Card() {
+    const [filppedStates , setFlippedStates] = useState({});
+    const flipHandler = (id) =>{
+        setFlippedStates((prevstate) => ({...prevstate, [id] : !filppedStates[id] })
+       )
+    }
   return (
     <>
         {CardsInfo.map((cardInfo) => (
-             <div className="card" key={cardInfo.id}>
+             <div className={`${filppedStates[cardInfo.id]? "flipped" : ""} card`} key={cardInfo.id} onClick={() => flipHandler(cardInfo.id)}>
              <div className="card-front"></div>
              <div className="card-back">{cardInfo.name}</div>
             </div>
@@ -15,4 +21,4 @@ function Card() {
   )
 }
 
-export default Card
+export default Card;
