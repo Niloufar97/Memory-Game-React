8002c36ba76e4553492ea4295261650b6da65f21
Niloufar <nilooooofar97@gmail.com>
2023-12-08 14:09:38 +0100

    random place for cards

--- memoryGame/src/components/cards/Card.jsx
+++ memoryGame/src/components/cards/Card.jsx
@@ -1,26 +1,39 @@
-import { useState } from "react"
-import { CardsInfo } from "./cardsInfo"
+import { useEffect, useState } from "react";
+import { CardsInfo } from "./cardsInfo";
+
 function Card() {
-    const [filppedStates , setFlippedStates] = useState({});
-    const flipHandler = (id) =>{
-        setFlippedStates((prevstate) => ({...prevstate, [id] : !filppedStates[id] })
-       )
-    }
+  const [filppedStates, setFlippedStates] = useState({});
+  const [shuffledCards , setShuffledCards] = useState([])
+  const flipHandler = (id) => {
+    setFlippedStates((prevstates) => ({
+      ...prevstates,
+      [id]: !filppedStates[id],
+    }));
+  };
+  const shuffleCards = () => {
+    const shuffled = [...CardsInfo].sort(() => Math.random() - 0.5)
+    setShuffledCards(shuffled);
+  }
+useEffect(() => {
+  shuffleCards()
+} , [])
+
   return (
     <>
-        {CardsInfo.map((cardInfo) => (
-             <div className={`${filppedStates[cardInfo.id]? "flipped" : ""} card`} key={cardInfo.id} onClick={() => flipHandler(cardInfo.id)}>
-             <div className="card-front"></div>
-             <div className="card-back">
-              <img src={cardInfo.img}></img>
-             </div>
-            </div>
-        )
-          
-       )}
-      
+      {shuffledCards.map((cardInfo) => (
+        <div
+          className={`${filppedStates[cardInfo.id] ? "flipped" : ""} card`}
+          key={cardInfo.id}
+          onClick={() => flipHandler(cardInfo.id)}
+        >
+          <div className="card-front"></div>
+          <div className="card-back">
+            <img src={cardInfo.img}></img>
+          </div>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
 export default Card;
