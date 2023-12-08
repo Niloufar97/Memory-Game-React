f7db48650287da5e9d27f27eaac67d9560277157
Niloufar <nilooooofar97@gmail.com>
2023-12-08 10:26:46 +0100

    update cards info

--- memoryGame/src/components/cards/cardsInfo.jsx
+++ memoryGame/src/components/cards/cardsInfo.jsx
@@ -1,26 +1,92 @@
+import bearImg from '../../assets/cardsImg/bear.svg'
+import crabImg from '../../assets/cardsImg/crab.svg'
+import dinosaurImg from '../../assets/cardsImg/dinosaur.svg'
+import elkImg from '../../assets/cardsImg/elk.svg'
+import zebraImg from '../../assets/cardsImg/zebra.svg'
+import shrimpImg from '../../assets/cardsImg/shrimp.svg'
+import penguinImg from '../../assets/cardsImg/penguin.svg'
+import raccoonImg from '../../assets/cardsImg/raccoon.svg'
 export const CardsInfo = [
     {
         id: 1,
-        name: "bear"
+        name: "bear", 
+        img: bearImg
+        
     },
     {
         id: 2,
-        name: "crab"
+        name: "bear", 
+        img: bearImg
+        
     },
     {
         id: 3,
-        name: "dinosaur"
+        name: "crab",
+        img: crabImg
     },
     {
         id: 4,
-        name: "elk"
+        name: "crab",
+        img: crabImg
     },
     {
         id: 5,
-        name: "fox"
+        name: "dinosaur",
+        img: dinosaurImg
     },
     {
         id: 6,
-        name: "heldgehog"
+        name: "dinosaur",
+        img: dinosaurImg
+    },
+    {
+        id: 7,
+        name: "elk",
+        img: elkImg
+    },
+    {
+        id: 8,
+        name: "elk",
+        img: elkImg
+    },
+    {
+        id: 9,
+        name: "zebra",
+        img: zebraImg
+    },
+    {
+        id: 10,
+        name: "zebra",
+        img: zebraImg
+    },
+    {
+        id: 11,
+        name: "shrimp",
+        img: shrimpImg
+    },
+    {
+        id: 12,
+        name: "shrimp",
+        img: shrimpImg
+    },
+    {
+        id: 13,
+        name: "penguin",
+        img: penguinImg
+    },
+    {
+        id: 14,
+        name: "penguin",
+        img: penguinImg
+    },
+    {
+        id: 15,
+        name: "raccoon",
+        img: raccoonImg
+    },
+    {
+        id: 16,
+        name: "raccoon",
+        img: raccoonImg
     },
 ]
