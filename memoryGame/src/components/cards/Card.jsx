3c39252c181f28fb8331ec81c47ced3dd2b92f3c
Niloufar <nilooooofar97@gmail.com>
2023-12-08 10:34:25 +0100

    add images  to cards

--- memoryGame/src/components/cards/Card.jsx
+++ memoryGame/src/components/cards/Card.jsx
@@ -11,7 +11,9 @@ function Card() {
         {CardsInfo.map((cardInfo) => (
              <div className={`${filppedStates[cardInfo.id]? "flipped" : ""} card`} key={cardInfo.id} onClick={() => flipHandler(cardInfo.id)}>
              <div className="card-front"></div>
-             <div className="card-back">{cardInfo.name}</div>
+             <div className="card-back">
+              <img src={cardInfo.img}></img>
+             </div>
             </div>
         )
           
